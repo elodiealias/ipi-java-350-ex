ae0a33c2cc540af78265b2734cebda0d73a42982
elodiealias <94829136+elodiealias@users.noreply.github.com>
2021-11-22 17:27:51 +0100

    Changement  "=" main.Yml

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -4,8 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.runners.Parameterized;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -46,7 +50,6 @@ class EmployeTest {
     @Test
     void getNombreAnneeWithPastAnciennete() {
         //Given
-
         Employe employe = new Employe("Dupont","Julien","T12345", LocalDate.now().minusYears(2), 2500.0, 1, 1.0);
         //When
         Integer nbAnnees = employe.getNombreAnneeAnciennete();
@@ -55,7 +58,30 @@ class EmployeTest {
 
     }
 
+    @Test
+    void getPrimeAnnuelle() {
+        //Given
+        Employe employe = new Employe("Dupont","Julien","M12345",LocalDate.now() ,2500.0, 1,1.0 );
+        //When
+        Double prime = employe.getPrimeAnnuelle();
+        //Then
+        Assertions.assertThat(prime).isEqualTo(1700d);
+    }
 
+    @ParameterizedTest(name = "matricule {0}, performance{1},tempsPartiel{2}, primeAnciennete{3} => prime{4} " )
+    @CsvSource({
+            "M12345,3, 1, 1.0, 1700.0",
+            "M12345,0, 1, 0.5, 850.0",
+            "M12345,1, 1, 1.0, 1800.0",
+    })
+    void TestgetPrimeAnnuelle(String matricule, Integer nbAnneeAnciennete, Integer performance, Double tempsPartiel, Double primeCalculee){
+        //Given, When, Then
+        //Matricule, DateEmbauche,Performance, TempsPartiel
+        Employe employe = new Employe("Dupont","Julien",matricule, LocalDate.now().minusYears(nbAnneeAnciennete),2500.0, performance,tempsPartiel);
+
+        Assertions.assertThat(primeCalculee).isEqualTo(primeCalculee);
+
+    }
 
 
 
